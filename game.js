f000803e8e769536f026943a41aba2ebeee37425
conveyGhost <zidanleopermana83@gmail.com>
2025-01-09 02:02:34 -0800

    initial commit

---
+++ game.js
@@ -0,0 +1,3 @@
+// NOTE: The cannon is located at the center of the scene and faces in the
+// positive direction of the z-axis. The x-axis is horizontal and the y-axis is
+// vertical relative to the cannon platform.
